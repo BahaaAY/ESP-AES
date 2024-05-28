b0b22d0f968aff6de1073a1d6e523bcfc827dcee
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-05-29 00:02:42 +0300

    first commit

---
+++ main/hello_world_main.c
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include <inttypes.h>
+#include <stdio.h>
+
+
+void app_main(void) {}
